7cb2c85d3d202f74acfb23ed6a1db7b9a1eb95f0
amir <sourena2575@gmail.com>
2020-03-12 11:27:06 +0330

    useRef

--- src/App.js
+++ src/App.js
@@ -1,53 +1,40 @@
-import React, { Children } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import propTypes from "prop-types";
 import "./App.css";
-const Test = props => {
-  return (
-    <div>
-      <h1>{props.str}</h1>
-      <h1>{props.bol ? "bool" : "nobool"}</h1>
-      <h1>{props.strOrNum}</h1>
-      {props.ary.map(item => {
-        return <li key={item}>{item}</li>;
-      })}
-      {props.ary0fObj.map(item => {
-        return <li key={item.age}>{item.name}</li>;
-      })}
-      <h1>{props.children}</h1>
-    </div>
-  );
-};
-
-Test.propTypes = {
-  str: propTypes.string,
-  bol: propTypes.bool,
-  strOrNum: propTypes.oneOfType([propTypes.string, propTypes.number]),
-  ary: propTypes.arrayOf(propTypes.number),
-  ary0fObj: propTypes.arrayOf(
-    propTypes.shape({
-      name: propTypes.string,
-      age: propTypes.number
-    })
-  ),
-  children: propTypes.isRequired
-};
 
 function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const subRef = useRef(null);
+  useEffect(() => {
+    firstNameRef.current.focus();
+  }, []);
+  const handlefirst = e => {
+    e.keyCode === 13
+      ? lastNameRef.current.focus()
+      : firstNameRef.current.focus();
+  };
+  const handlelast = e => {
+    e.keyCode === 13 ? subRef.current.focus() : lastNameRef.current.focus();
+  };
+  const handlesub = () => {};
   return (
     <div className="App">
-      <Test
-        str="tech"
-        bol={true}
-        strOrNum={10}
-        ary={[1, 2, 3]}
-        ary0fObj={[
-          { name: "a", age: 10 },
-          { name: "f", age: 20 }
-        ]}
-      >
-        <div>Child</div>
-        <div>Child</div>
-      </Test>
+      <input
+        type="text"
+        ref={firstNameRef}
+        placeholder="enter fisrt"
+        onKeyDown={handlefirst}
+      />
+      <input
+        type="text"
+        placeholder="enter"
+        ref={lastNameRef}
+        onKeyDown={handlelast}
+      />
+      <button ref={subRef} onKeyDown={handlesub}>
+        click
+      </button>
     </div>
   );
 }
